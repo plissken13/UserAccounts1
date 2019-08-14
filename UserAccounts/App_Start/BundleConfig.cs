7a1e6d9d668d2b2ebe6ba5a87cc6aca1f4babe58
Alisa <alisciep@gmail.com>
2019-08-14 13:48:31 +0300

    Redesign1

--- UserAccounts/App_Start/BundleConfig.cs
+++ UserAccounts/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace UserAccounts
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/UserAccounts").Include(
+                "~/Scripts/app/UserActions.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
